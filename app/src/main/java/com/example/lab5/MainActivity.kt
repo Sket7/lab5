77371f2855fd6d4acc47bbf808d6ca5bd3fc68f1
Egor Sket <egor1212123@mail.ru>
2024-03-27 18:00:24 +0700

    Выбор срока готов

--- app/src/main/java/com/example/lab5/MainActivity.kt
+++ app/src/main/java/com/example/lab5/MainActivity.kt
@@ -3,13 +3,29 @@ package com.example.lab5
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lab5.ui.theme.Lab5Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +38,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Ded")
+                    Greeting()
                 }
             }
         }
@@ -30,17 +46,68 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting() {
+    val first = "Введите сумму"
+    val second = "Выберите срок"
+    val listRadio = listOf("3 месяцев (3%)", "6 месяцев (5%)", "12 месяцев (9%)")
+
+    var number by remember {
+        mutableIntStateOf(0)
+    }
+    var textF by remember {
+        mutableStateOf("")
+    }
+    val (selectedOption, onOptionSelected) = remember { mutableStateOf(listRadio[0]) }
+
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(15.dp)
+    ) {
+        Row(
+            Modifier.fillMaxWidth(),
+            Arrangement.SpaceBetween,
+            Alignment.CenterVertically
+        ) {
+            Text(first, Modifier.fillMaxWidth(0.4f))
+            TextField(
+                value = textF,
+                onValueChange = { text ->
+                    textF = text.filter { it.isDigit() }
+                },
+                Modifier.fillMaxWidth(),
+            )
+        }
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(0.dp, 10.dp, 0.dp, 10.dp),
+            Arrangement.Start,
+            Alignment.Top
+        ) {
+            Text(text = second)
+            Column(Modifier.selectableGroup()) {
+                listRadio.forEach { radioText ->
+                    Row(
+                        Modifier.fillMaxWidth().height(56.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        RadioButton(
+                            (radioText == selectedOption),
+                            { onOptionSelected(radioText) })
+                        Text(radioText)
+                    }
+
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Lab5Theme {
-        Greeting("Android")
+        Greeting()
     }
 }
